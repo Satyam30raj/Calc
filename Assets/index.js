2fc1506eb86ad6930fa95e0b9ff12b14136ebe8c
Satyam Raj <175140440+Satyam30raj@users.noreply.github.com>
2024-08-06 21:37:48 +0530

    index.js

--- Assets/index.js
+++ Assets/index.js
@@ -1,34 +1,28 @@
-function Solve(val) {
-    var v = document.getElementById('res');
-    v.value += val;
- }
- function Result() {
-    var num1 = document.getElementById('res').value;
-    try {
-       var num2 = eval(num1.replace('x', '*'));
-       document.getElementById('res').value = num2;
-    } catch {
-       document.getElementById('res').value = 'Error';
-    }
- }
- function Clear() {
-    var inp = document.getElementById('res');
-    inp.value = '';
- }
- function Back() {
-    var ev = document.getElementById('res');
-    ev.value = ev.value.slice(0, -1);
- }
- document.addEventListener('keydown', function (event) {
-    const key = event.key;
-    const validKeys = '0123456789+-*/.%';
-    if (validKeys.includes(key)) {
-       Solve(key === '*' ? 'x' : key);
-    } else if (key === 'Enter') {
-       Result();
-    } else if (key === 'Backspace') {
-       Back();
-    } else if (key.toLowerCase() === 'c') {
-       Clear();
-    }
- });
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+
+const calculate = (btnValue) => {
+  display.focus();
+  if (btnValue === "=" && output !== "") {
+  
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+
+    output = output.toString().slice(0, -1);
+  } else {
+
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
+
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
